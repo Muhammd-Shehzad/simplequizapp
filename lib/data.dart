6a72dc848dc7b444bad9391f90ad82f936122316
Muhammd-Shehzad <127086042+Muhammd-Shehzad@users.noreply.github.com>
2024-07-18 22:39:37 +0500

    done

--- lib/data.dart
+++ lib/data.dart
@@ -1,38 +1,29 @@
 List<String> Question = [
-  'Q1.Who created Flutter?',
-  'Q2.What is Flutter?',
-  'Q3. Which programming language is used by Flutte',
-  'Q4. Who created Dart programming language?',
+  'Which of the following signal molecules does not interact with cell surface receptors?',
+  'Which of the following G-protein takes part in the regulation of vision?',
+  'Extracellular signaling in animals is carried out by ...',
+  'How many types of cell signalings are there?',
 ];
 
 List<String> op1 = [
-  'A)Facebook',
-  'A)Android Development Kit',
-  ''
-      'B)Android Development Kit',
-  'A)Ruby',
-  'A)Lars Bak and Kasper Lund'
+  'Insulin',
+  'Golf',
+  'Autocrine',
+  '4',
 ];
 List<String> op2 = [
-  'B)Adobe',
-  'B)IOS Development Kit',
-  ''
-      'B)Dart',
-  'B)Brendan Eich',
+  'Gastrin',
+  'Gi family',
+  'Endocrine',
+  '5',
 ];
-List<String> op3 = [
-  'A)Facebook',
-  'B)Android Development Kit',
-  ''
-      'B)Android Development Kit',
-  'C)Ruby',
-  'D)Lars Bak and Kasper Lund'
-];
-List<String> op4 = [
-  'A)Facebook',
-  'B)Android Development Kit',
-  ''
-      'B)Android Development Kit',
-  'C)Ruby',
-  'D)Lars Bak and Kasper Lund'
+List<String> op3 = ['Glucagon', 'Gs family', 'Juxtacrine', '7'];
+List<String> op4 = ['Testosterone', 'Gq family', 'All of the above', '8'];
+
+List<String> atempAns = [];
+List<String> correctAns = [
+  'Testosterone',
+  'Gi family',
+  'All of the above',
+  '4'
 ];
--- lib/screen1.dart
+++ lib/screen1.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:simplequizapp/result_page.dart';
 import 'data.dart';
 
 class Screen1 extends StatefulWidget {
@@ -14,108 +13,250 @@ class _Screen1State extends State<Screen1> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: 4,
-                itemBuilder: ((context, index) {
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 5, vertical: 5),
-                        child: Container(
-                          height: 70,
-                          width: 400,
-                          decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(color: Colors.black)),
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              '${Question[index]}',
-                              style: TextStyle(
-                                  color: Colors.black,
-                                  fontSize: 16,
-                                  fontWeight: FontWeight.w600),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.indigo, Colors.deepPurple],
+                end: Alignment.topLeft,
+                begin: Alignment.bottomLeft)),
+        child: Column(
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: ListView.builder(
+                  itemCount: 4,
+                  itemBuilder: ((context, index) {
+                    return Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 5, vertical: 5),
+                          child: GestureDetector(
+                            onTap: () {},
+                            child: Container(
+                              height: 70,
+                              width: 400,
+                              decoration: BoxDecoration(
+                                  gradient: LinearGradient(
+                                      colors: [
+                                        Colors.indigo,
+                                        Colors.deepPurple
+                                      ],
+                                      end: Alignment.topLeft,
+                                      begin: Alignment.bottomLeft),
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(color: Colors.black)),
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Q${index + 1}: ${Question[index]}',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                              ),
                             ),
                           ),
                         ),
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 150,
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(color: Colors.black)),
-                            child: Text('${op1[index]}'),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 150,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(color: Colors.black),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            GestureDetector(
+                              onTap: () {
+                                atempAns.add(op1[index]);
+                                print('op1');
+                              },
+                              child: Container(
+                                height: 60,
+                                width: 180,
+                                decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          Colors.indigo,
+                                          Colors.deepPurple
+                                        ],
+                                        end: Alignment.topLeft,
+                                        begin: Alignment.bottomLeft),
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(color: Colors.black)),
+                                child: Align(
+                                    alignment: Alignment.topLeft,
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'A) ${op1[index]}',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 18),
+                                      ),
+                                    )),
+                              ),
                             ),
-                            child: Text('${op2[index]}'),
-                          ),
-                        ],
-                      ),
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            height: 50,
-                            width: 150,
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(color: Colors.black)),
-                            child: Text('${op3[index]}'),
-                          ),
-                          Container(
-                            height: 50,
-                            width: 150,
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(10),
-                                border: Border.all(color: Colors.black)),
-                            child: Text('${op4[index]}'),
-                          ),
-                        ],
-                      ),
-                    ],
-                  );
-                }),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: GestureDetector(
+                                onTap: () {
+                                  atempAns.add(op2[index]);
+                                  print('opt2');
+                                },
+                                child: Container(
+                                  height: 60,
+                                  width: 180,
+                                  decoration: BoxDecoration(
+                                    gradient: LinearGradient(
+                                        colors: [
+                                          Colors.indigo,
+                                          Colors.deepPurple
+                                        ],
+                                        end: Alignment.topLeft,
+                                        begin: Alignment.bottomLeft),
+                                    borderRadius: BorderRadius.circular(10),
+                                    border: Border.all(color: Colors.black),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('B) ${op2[index]}',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: GestureDetector(
+                                onTap: () {
+                                  atempAns.add(op3[index]);
+                                  print('opt3');
+                                },
+                                child: Container(
+                                  height: 60,
+                                  width: 180,
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                          colors: [
+                                            Colors.indigo,
+                                            Colors.deepPurple
+                                          ],
+                                          end: Alignment.topLeft,
+                                          begin: Alignment.bottomLeft),
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(color: Colors.black)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: Text('C) ${op3[index]}',
+                                          style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.w600)),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topLeft,
+                              child: GestureDetector(
+                                onTap: () {
+                                  atempAns.add(op4[index]);
+                                  print('op4');
+                                },
+                                child: Container(
+                                  height: 60,
+                                  width: 180,
+                                  decoration: BoxDecoration(
+                                      gradient: LinearGradient(
+                                          colors: [
+                                            Colors.indigo,
+                                            Colors.deepPurple
+                                          ],
+                                          end: Alignment.topLeft,
+                                          begin: Alignment.bottomLeft),
+                                      borderRadius: BorderRadius.circular(10),
+                                      border: Border.all(color: Colors.black)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(8.0),
+                                    child: Text('D) ${op4[index]}',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 18,
+                                            fontWeight: FontWeight.w600)),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                      ],
+                    );
+                  }),
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              height: 50,
-              width: 150,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  border: Border.all(color: Colors.black)),
-              child: Center(child: Text('Submit')),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                onTap: () {
+                  var marks = 0;
+                  for (int i = 0; i <= correctAns.length; i++) {
+                    if (atempAns[i] == correctAns.length) {
+                      print({'Correct'});
+                      marks++;
+                    } else {
+                      print({'Wrong'});
+                    }
+                  }
+                  print('This is Atemp Answer $atempAns');
+
+                  // Navigator.push(context,
+                  //     MaterialPageRoute(builder: (context) => ResultPage()));
+                },
+                child: Container(
+                  height: 60,
+                  width: 200,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.indigo, Colors.deepPurple],
+                        end: Alignment.topLeft,
+                        begin: Alignment.bottomLeft),
+                    borderRadius: BorderRadius.circular(10),
+                    border: Border.all(color: Colors.black),
+                  ),
+                  child: Center(
+                      child: Text(
+                    'Submit',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold),
+                  )),
+                ),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
